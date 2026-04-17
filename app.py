238c505e539442ccb30efbf81e604becd7ca02e9
Eloyhasan <hasan.arslan82@gmail.com>
2026-04-17 12:35:53 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,39 +1,4 @@
 import streamlit as st
-import pandas as pd
 
-st.title("Kişisel Veri Takip")
-
-# veri giriş alanı
-date = st.date_input("Tarih")
-weight = st.number_input("Kilo", step=0.1)
-steps = st.number_input("Adım")
-mood = st.slider("Mood (1-10)", 1, 10)
-notes = st.text_input("Not")
-
-if st.button("Kaydet"):
-    data = {
-        "Date": date,
-        "Weight": weight,
-        "Steps": steps,
-        "Mood": mood,
-        "Notes": notes
-    }
-
-    df = pd.DataFrame([data])
-
-    try:
-        existing = pd.read_csv("data.csv")
-        df = pd.concat([existing, df])
-    except:
-        pass
-
-    df.to_csv("data.csv", index=False)
-
-    st.success("Kayıt eklendi")
-
-# veri göster
-try:
-    df = pd.read_csv("data.csv")
-    st.dataframe(df)
-except:
-    st.info("Henüz veri yok")
+st.title("Test OK")
+st.write("Eğer bunu görüyorsan sistem çalışıyor")
